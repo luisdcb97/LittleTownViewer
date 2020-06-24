116dc2dc796676a541fd615da717599f9814450c
Luis <luisdcb97@gmail.com>
2020-06-24 21:34:04 +0100

    Add screen to list saves Changed logger formatter

--- src/littletownviewer/MySketch.java
+++ src/littletownviewer/MySketch.java
@@ -1,16 +1,18 @@
 package littletownviewer;
 
 import littletownviewer.ui.Notification;
+import littletownviewer.ui.Screen;
+import littletownviewer.ui.screens.SavesListMenu;
 import processing.core.PApplet;
 
 import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Handler;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 public class MySketch extends PApplet{
     Notification notificationTray;
     int i = 0;
+
+    private Screen currentScreen, previousScreen;
 
     public void settings(){
         size(1200, 675);
@@ -18,18 +20,25 @@ public class MySketch extends PApplet{
 
     public void setup(){
         notificationTray = new Notification(this, "ola");
+        currentScreen = new SavesListMenu(this);
+        currentScreen.setup();
     }
 
     public void draw(){
-        background(15);
+        currentScreen.draw();
+
         if(notificationTray.isShowing()){
             notificationTray.draw();
         }
     }
 
-    public void mouseClicked(){
+    @Override
+    public void mousePressed(){
         if(notificationTray.isShowing()){
             notificationTray.mouseClicked();
+        }
+        else{
+            currentScreen.mousePressed();
         }
     }
 
@@ -44,6 +53,9 @@ public class MySketch extends PApplet{
         Handler fh = null;
         try {
             fh = new FileHandler("littleTownViewer.log");
+            fh.setLevel(Level.INFO);
+            Formatter fmt = Logger.getLogger("").getHandlers()[0].getFormatter();
+            fh.setFormatter(fmt);
             Logger.getLogger("").addHandler(fh);
             PApplet.main(processingArgs);
         } catch (IOException e) {
---
+++ src/littletownviewer/ui/screens/SavesListMenu.java
@@ -0,0 +1,99 @@
+package littletownviewer.ui.screens;
+
+import littletownviewer.SaveFileManager;
+import littletownviewer.ui.Screen;
+import littletownviewer.ui.buttons.SaveFileButton;
+import org.jetbrains.annotations.NotNull;
+import processing.core.PApplet;
+import processing.event.KeyEvent;
+import processing.event.MouseEvent;
+
+public class SavesListMenu extends Screen {
+    private static final int NUM_SAVES = 3;
+
+
+    private SaveFileButton saveButtons[];
+
+    public SavesListMenu(@NotNull PApplet window){
+        this.setWindow(window);
+        this.saveButtons = new SaveFileButton[NUM_SAVES];
+        for (int i = 0; i < NUM_SAVES; i++){
+            SaveFileManager save = new SaveFileManager(window, i);
+            this.saveButtons[i] =
+                    new SaveFileButton(window, save, 200, 50 + i * 200,
+                            800, 150);
+        }
+    }
+
+    @Override
+    public void setup() {
+        for (SaveFileButton saveButton : this.saveButtons){
+            saveButton.setup();
+        }
+    }
+
+    @Override
+    public void draw() {
+        window.background(50);
+
+        for (SaveFileButton saveButton : this.saveButtons){
+            saveButton.draw();
+        }
+    }
+
+    @Override
+    public void keyTyped() {}
+
+    @Override
+    public void keyTyped(KeyEvent event) {}
+
+    @Override
+    public void keyPressed() {}
+
+    @Override
+    public void keyPressed(KeyEvent event) {}
+
+    @Override
+    public void keyReleased() {}
+
+    @Override
+    public void keyReleased(KeyEvent event) {}
+
+    @Override
+    public void mouseClicked() {}
+
+    @Override
+    public void mouseClicked(MouseEvent event) {}
+
+    @Override
+    public void mousePressed() {
+        for (SaveFileButton saveButton : this.saveButtons){
+            if(saveButton.isMouseOver(window.mouseX, window.mouseY))
+            {
+                saveButton.mouseClicked();
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent event) {}
+
+    @Override
+    public void mouseReleased() {}
+
+    @Override
+    public void mouseReleased(MouseEvent event) {}
+
+    @Override
+    public void MouseDragged() {}
+
+    @Override
+    public void MouseDragged(MouseEvent event) {}
+
+    @Override
+    public void MouseWheel() {}
+
+    @Override
+    public void MouseWheel(MouseEvent event) {}
+
+}
